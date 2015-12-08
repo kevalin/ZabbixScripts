512aea692bc1622c42cdaa6098e4785325bb1dbf
Kevalin <kevalin123@gmail.com>
2015-12-08 11:27:33 +0800

    Create check_net_status.py

---
+++ check_net_status.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+import json
+
+f = open("/etc/zabbix/scripts/need_check_ip.txt")
+dictIp = {}
+infos = []
+
+for line in f:
+    tempObj = {}
+    tempObj["{#IP}"] = line.replace('\n', '').replace(' ', '')
+    infos.append(tempObj)
+
+dictIp["data"] = infos
+print json.dumps(dictIp, sort_keys=True, indent=4)
